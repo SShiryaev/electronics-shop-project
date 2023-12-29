67e0b711a62c792e2cc1b8b15f3d5eece0eda9ed
Sergey Shiryaev <shiryaev_fox@mail.ru>
2023-12-29 20:06:09 +0300

    Add tests for the new methods.

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,18 @@ def test_apply_discount(item1_fixture):
     Item.pay_rate = 0.5
     item1_fixture.apply_discount()
     assert item1_fixture.price == 40000
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    item1 = Item.all[1]
+    assert item1.name == 'Ноутбук'
+    assert item1.price == 1000
+    assert item1.quantity == 3
+
+
+def test_string_to_number():
+    assert Item.string_to_number('7') == 7
+    assert Item.string_to_number('2.0') == 2
+    assert Item.string_to_number('3.9') == 3
