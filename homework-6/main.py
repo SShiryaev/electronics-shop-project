ecfcf14c00843b15f9ce6e2ad1631defabd67cf0
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-01-29 00:40:21 +0300

    Fixed bugs#2

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,11 +1,10 @@
 from src.item import Item
-from src.config import VALID_CSV, DAMAGE_CSV, EMPTY_PATH
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv(EMPTY_PATH)
+    Item.instantiate_from_csv()
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv(DAMAGE_CSV)
+    Item.instantiate_from_csv()
     # InstantiateCSVError: Файл item.csv поврежден
